f49fc456fca7bac90620b910184577e98125dff4
daptanque <daniel.apeixoto.tanque@gmail.com>
2024-03-14 17:24:22 +0000

    Presentation Layer - MainActivity Layout

--- app/src/main/java/com/example/movieapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/example/movieapplication/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.movieapplication
+package com.example.movieapplication.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.movieapplication.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
