39bf0dbde3d3062e45dc58bbb475f4ccf632e579
daptanque <daniel.apeixoto.tanque@gmail.com>
2024-03-15 08:54:29 +0000

    Update Menu and Update Movies function

--- app/src/main/java/com/example/movieapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/example/movieapplication/presentation/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.movieapplication.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
@@ -58,7 +61,38 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+    }
 
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.update,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            R.id.action_update -> {
+                updateMovies()
+                true
+            }else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun updateMovies() {
+        binding.movieProgressBar.visibility = View.VISIBLE
+        val response = movieViewModel.updateMovies()
+        response.observe(this, Observer {
+
+            if(it != null){
+                adapter.setList(it)
+                adapter.notifyDataSetChanged()
+                binding.movieProgressBar.visibility = View.GONE
+            }else{
+                binding.movieProgressBar.visibility = View.GONE
+            }
+
+        })
     }
 
 
